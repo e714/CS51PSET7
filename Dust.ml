9e9ba3de02f4ed04418653855ca5750567444586
shichao13 <shichao.ma13@gmail.com>
2014-04-10 20:47:55 -0400

    Die fix.

--- Dust.ml
+++ Dust.ml
@@ -14,6 +14,6 @@ object (self)
   (* ### TODO: Part 4 Aging ### *)
   
   method draw_picture = self#draw_circle (Graphics.rgb 150 150 150)
-         Graphics.black (String.sub name 0 1)
+         Graphics.black (String.sub name 0 2)
   
 end
